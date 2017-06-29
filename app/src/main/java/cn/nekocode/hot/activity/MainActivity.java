d3254016084e983f20b4377d22fc89c6056b0295
nekocode <nekocode.cn@gmail.com>
2017-06-29 17:38:35 +0800

    Add fabric and other libs

--- app/src/main/java/cn/nekocode/hot/activity/MainActivity.java
+++ app/src/main/java/cn/nekocode/hot/activity/MainActivity.java
@@ -1,4 +1,21 @@
-package cn.nekocode.hot;
+/*
+ * Copyright (C) 2017 nekocode (nekocode.cn@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cn.nekocode.hot.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +24,12 @@ import android.widget.Toast;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 
+import cn.nekocode.hot.R;
 import cn.nekocode.hot.luaj.HotLuaGlobals;
 
+/**
+ * @author nekocode (nekocode.cn@gmail.com)
+ */
 public class MainActivity extends AppCompatActivity {
     private HotLuaGlobals luaGlobals;
 
