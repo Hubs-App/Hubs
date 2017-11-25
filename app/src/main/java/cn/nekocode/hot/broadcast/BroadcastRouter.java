d24709ed9cb4cd00f1f02b02eef0a70c2b88f807
nekocode <nekocode.cn@gmail.com>
2017-11-25 17:25:49 +0800

    Add broadcast router interface

--- app/src/main/java/cn/nekocode/hot/broadcast/BroadcastRouter.java
+++ app/src/main/java/cn/nekocode/hot/broadcast/BroadcastRouter.java
@@ -15,16 +15,22 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package cn.nekocode.hot;
+package cn.nekocode.hot.broadcast;
 
 import android.content.Context;
+import android.os.Parcelable;
 
+import java.util.ArrayList;
+
+import cn.nekocode.hot.BuildConfig;
+import cn.nekocode.hot.Constants;
 import cn.nekocode.hot.data.model.Column;
 import cn.nekocode.hot.ui.browser.BrowserActivity;
 import cn.nekocode.hot.ui.setting.ColumnConfigActivity;
 import cn.nekocode.meepo.Meepo;
 import cn.nekocode.meepo.annotation.Bundle;
 import cn.nekocode.meepo.annotation.Query;
+import cn.nekocode.meepo.annotation.TargetAction;
 import cn.nekocode.meepo.annotation.TargetClass;
 import cn.nekocode.meepo.annotation.TargetPath;
 import cn.nekocode.meepo.config.UriConfig;
@@ -32,27 +38,20 @@ import cn.nekocode.meepo.config.UriConfig;
 /**
  * @author nekocode (nekocode.cn@gmail.com)
  */
-public interface ActivityRouter {
-    ActivityRouter IMPL = new Meepo.Builder()
-            .config(new UriConfig().scheme(BuildConfig.SCHEME).host(BuildConfig.APPLICATION_ID))
-            .build().create(ActivityRouter.class);
+public interface BroadcastRouter {
+    BroadcastRouter IMPL = new Meepo.Builder()
+            .config(new BroadcastConfig()).build().create(BroadcastRouter.class);
 
 
-    @TargetPath("browser")
-    boolean gotoBrowser(Context context, @Query("url") String url);
+    @TargetAction(Constants.ACTION_NOTIFY_COLUMN_INSTALLED)
+    boolean tellColumnInstalled(Context context, @Bundle(Constants.ARG_COLUMNS) ArrayList<Parcelable> columns);
 
-    @TargetPath("browser")
-    boolean gotoBrowser(Context context, @Query("column_id") String columnId, @Query("url") String url);
+    @TargetAction(Constants.ACTION_NOTIFY_COLUMN_UNINSTALLED)
+    boolean tellColumnUninstalled(Context context, @Bundle(Constants.ARG_COLUMNS) ArrayList<Parcelable> columns);
 
-    @TargetClass(BrowserActivity.class)
-    boolean gotoBrowser(Context context, @Bundle("column") Column column, @Bundle("url") String url);
+    @TargetAction(Constants.ACTION_NOTIFY_COLUMN_PREFERENCE_CHANGED)
+    boolean tellColumnPreferenceChanged(Context context, @Bundle(Constants.ARG_COLUMNS) ArrayList<Parcelable> columns);
 
-    @TargetPath("setting")
-    boolean gotoSetting(Context context);
-
-    @TargetPath("column_manager")
-    boolean gotoColumnManager(Context context);
-
-    @TargetClass(ColumnConfigActivity.class)
-    boolean gotoColumnConfig(Context context, @Bundle("column") Column column);
+    @TargetAction(Constants.ACTION_NOTIFY_COLUMN_CONFIG_CHANGED)
+    boolean tellColumnConfigChanged(Context context, @Bundle(Constants.ARG_COLUMNID) String columnId);
 }
